f9c52030dc460bd2318cbf483f7190c624d395f0
Nikhil Srivastava <nikhilsrivastava@Nikhils-Air.Dlink>
2020-10-30 10:16:32 +0530

    used react hooks to maintain state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Bring some coke form the market','Bring the veggies']);
+  const [input, setInput] = useState('');
+  const addTodo=(event)=>{
+    setTodos([...todos,input]);
+  }
   return (
     <div className="App">
       <h1>hi</h1>
+      <input value={input} onChange={event=> setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo)=>{
+          return <li>{todo}</li>
+        })}
+      </ul>
     </div>
   );
 }
